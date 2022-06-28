764ecbee84ca076d51526d86dd29276c34d13ec3
Jung_Ji_Bum <2377325@naver.com>
2022-06-28 22:16:35 +0900

    User Interface Commit

--- PMS_Proj/Login_Main.cs
+++ PMS_Proj/Login_Main.cs
@@ -13,16 +13,22 @@ namespace PMS_Proj
             string ID = "root";
             string PW = "root";
 
-            if (Id_Field.Text == ID)
+            if (Id_Field.Text != ID)
             {
-                if (Pw_Field.Text == PW)
-                {
-                    MessageBox.Show("로그인성공");
-                    this.Visible = false;
-                    Main_UI main_UI = new Main_UI();
-                    main_UI.ShowDialog();
-                }
+                MessageBox.Show("아이디가 틀렸습니다.");
             }
+            else if (Id_Field.Text == ID && Pw_Field.Text != PW)
+            {
+                MessageBox.Show("비밀번호가 틀렸습니다.");
+            }
+            else if(Id_Field.Text ==ID && Pw_Field.Text == PW)
+            {
+                MessageBox.Show("로그인성공");
+                this.Visible = false;
+                Main_UI main_UI = new Main_UI();
+                main_UI.ShowDialog();
+            }
+
         }
 
         private void SignUp_Btn_Click(object sender, EventArgs e)
@@ -30,7 +36,7 @@ namespace PMS_Proj
             this.Visible = false;
             SignUp signup = new SignUp();
             signup.ShowDialog();
-            
+
         }
     }
 }
